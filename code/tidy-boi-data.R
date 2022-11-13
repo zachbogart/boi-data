0722e5c18cb2acb7535080c03790d33563b0115e
Zach Bogart <>
2022-11-12 18:18:45 -0800

    export boi achievement data

--- code/tidy-boi-data.R
+++ code/tidy-boi-data.R
@@ -19,7 +19,22 @@ tidy <- raw %>%
         col = ((row_number() - 1) %% 3) + 1,
         name = colnames_unlocks[col]
     ) %>% 
-    mutate(tmp = col == 1, num = cumsum(tmp)) %>% 
-    select(-c(col, tmp)) %>% 
-    pivot_wider(names_from = name, values_from = raw)
+    mutate(num = cumsum(col == 1)) %>% 
+    select(-c(col)) %>% 
+    pivot_wider(names_from = name, values_from = raw) %>% 
+    mutate(timestamp = str_replace(timestamp, "Unlocked ", ""))
+
+export <- tidy %>% 
+    # add in the year for recent achievements
+    mutate(timestamp = case_when(
+        !str_detect(timestamp, ",") ~ paste0(str_extract(timestamp, "[A-Za-z]+ [0-9]+"), ", 2022", str_extract(timestamp, " @.*$")),
+        TRUE ~ timestamp
+    )) %>% 
+    # parse timestamp as datetime
+    mutate(datetime = parse_datetime(timestamp, "%b %d, %Y @ %I:%M%p")) %>% 
+    relocate(datetime, .before = description) %>% 
+    arrange(datetime)
     
+# save off as a CSV
+export %>% 
+    write_csv(here("outputs/boi-achievements.csv"))
